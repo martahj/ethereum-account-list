0ff9ed8361cdaa9a134c4c19bb552fe94debbd5e
Marta Johnson <martajohnson@Martas-MacBook-Pro.local>
2022-04-27 16:30:07 -0500

    Removes warnings

--- src/utils/etherscanApi.js
+++ src/utils/etherscanApi.js
@@ -5,7 +5,7 @@ async function getEth(address) {
     const result = await fetch(`https://api.etherscan.io/api?module=account&action=balance&address=${address}&tag=latest&apikey=${etherscanApiKey}`)
       .then(result => result.json())
     if (result.status !== '1') {
-        console.warn(`Failed to fetch ETH for address ${address}`);
+        // console.warn(`Failed to fetch ETH for address ${address}`);
         return 0;
     }
     return parseInt(result.result, 10);
@@ -15,7 +15,7 @@ async function getBlockNumber() {
     const d = Math.ceil(Date.now() / 1000);
     const result = await fetch(`https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp=${d}&closest=before&apikey=${etherscanApiKey}`).then(r => r.json())
     if (result.status !== '1') {
-        console.warn(`Failed to fetch latest block number`);
+        // console.warn(`Failed to fetch latest block number`);
         return 0;
     }
     return parseInt(result.result, 10);
@@ -25,7 +25,7 @@ async function getNormalTransactions(address, startBlock, endBlock, numberToGet,
     const result = await fetch(`https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=${startBlock}&endblock=${endBlock}&page=${page}&offset=${numberToGet}&sort=asc&apikey=${etherscanApiKey}`)
       .then(r => r.json());
     if (result.status !== '1') {
-        console.warn(`Failed to fetch normal transactions for address ${address}`);
+        // console.warn(`Failed to fetch normal transactions for address ${address}`);
         return []
     }
     return result.result;
@@ -35,7 +35,7 @@ async function getInternalTransactions(address, startBlock, endBlock, numberToGe
     const result = await fetch(`https://api.etherscan.io/api?module=account&action=txlistinternal&address=${address}&startblock=${startBlock}&endblock=${endBlock}&page=${page}&offset=${numberToGet}&sort=asc&apikey=${etherscanApiKey}`)
       .then(r => r.json());
     if (result.status !== '1') {
-        console.warn(`Failed to fetch internal transactions for address ${address}`);
+        // console.warn(`Failed to fetch internal transactions for address ${address}`);
         return []
     }
     return result.result;
